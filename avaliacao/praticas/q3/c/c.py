84981a133c7c2b55087f92451ce43202e9b6ee56
Aline Correia <afcs.cin@gmail.com>
2024-11-05 08:46:43 -0300

    FEAT: Adicionando imagens

--- avaliacao/praticas/q3/c/c.py
+++ avaliacao/praticas/q3/c/c.py
@@ -1,23 +1,45 @@
 import cv2
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 def verificar_borrado(imagem):
     img = cv2.imread(imagem, 0)
     
-    # Aplicar o filtro Sobel para detectar bordas
-    sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
-    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    # Carregar a imagem 
+    img = cv2.imread(imagem, 0)
+
+    # Aplicar o filtro laplaciano, para detectar as bordas
+    laplaciano = cv2.Laplacian(img, cv2.CV_64F)
+  
+    # binarizar a imagem para a posterior con
+    _, imagem_bin = cv2.threshold(laplaciano, 50, 255, cv2.THRESH_BINARY)
+
+    # Calcular a soma dos pixels brancos que seriam as bordas
+    soma_brancos = np.sum(imagem_bin == 255)
     
-    #calcular a magnitude das bordas
-    mag_borda = np.sqrt(sobel_x**2 + sobel_y**2)
+    # estou plotando algumas imagens para referência
+    plt.figure(figsize=(6.4*5, 4.8*5), constrained_layout=False)
+
+    plt.subplot(1, 3, 1)
+    plt.title("Imagem inicial")
+    plt.imshow(img, cmap="gray")  
+    plt.axis('off')
+            
+    plt.subplot(1, 3, 2)
+    plt.title("Imagem com laplaciano")
+    plt.imshow(laplaciano, cmap="gray")
+    plt.axis('off')
+   
+    plt.subplot(1, 3, 3)
+    plt.title("Imagem binarizada")
+    plt.imshow(imagem_bin, cmap="gray")  
+    plt.axis('off')
     
-    #calcular a média das intensidades das bordas
-    media_borda = np.mean(mag_borda)
-    print(media_borda)
-    
-    # Se a média é baixa, a imagem provavelmente está borrada
-    return media_borda < 10  # Experimental
+    plt.show()
+
+    # Se a quantidade está baixa, a imagem provavelmente está borrada
+    return soma_brancos < 10  
 
 
 for imagem in os.listdir("avaliacao\praticas\q3\CME_01"):
